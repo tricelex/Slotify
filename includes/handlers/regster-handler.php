c10539f7e18ff6e9746e4549343a879503c61377
Chuckz Okoye <tricelex@gmail.com>
2019-09-28 07:52:23 +0100

    Added basic valdation functions in account class

--- includes/handlers/regster-handler.php
+++ includes/handlers/regster-handler.php
@@ -18,26 +18,6 @@
     return $inputText;
   }
 
-  function validateUsername($un) {
-
-  }
-
-  function validateFirstName($fn) {
-
-  }
-
-  function validateLastName($ln) {
-
-  }
-
-  function validateEmails($em, $em2) {
-
-  }
-
-  function validatePasswords($pw, $pw2) {
-
-  }
-
   if (isset($_POST['registerButton'])) {
     $username = sanitzeFormUsername($_POST['username']);
     $firstName = sanitzeFormUsername($_POST['firstName']);
@@ -47,10 +27,6 @@
     $password = sanitzeFormPassword($_POST['password']);
     $password2 = sanitzeFormPassword($_POST['password2']);
 
-    validateUsername($username);
-    validateFirstName($firstName);
-    validateUsername($lastName);
-    validateEmails($email, $email2);
-    validatePasswords($password, $password2);
+    $account->register($username, $firstName, $lastName, $email, $email2, $password, $password2);
   }
 ?>
