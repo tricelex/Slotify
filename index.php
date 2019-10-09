47f5e7b22fad8944e60e945bc69b41925d940117
Chuckz Okoye <tricelex@gmail.com>
2019-10-10 00:34:04 +0100

    added control buttons to index page

--- index.php
+++ index.php
@@ -1,22 +1,81 @@
 <?php
   include 'includes/config.php';
-  print_r($_SESSION);
-//  session_destroy();
+  //  session_destroy();
 
   if (isset($_SESSION['userLoggedIn'])) {
     $userLoggedn = $_SESSION['userLoggedIn'];
   } else {
-    header("Location: register.php");
+//    header("Location: register.php");
   }
 ?>
 
 <html>
   <head>
     <title>Welcome to Slotify!</title>
+    <link rel="stylesheet" type="text/css" href="assets/css/style.css">
   </head>
 
   <body>
-    Hello!
+
+    <div id="nowPlayingBarContainer">
+      <div id="nowPlayingBar">
+
+        <div id="nowPlayingLeft">
+          <div class="content">
+            <span class="albumLink">
+              <img src="assets/images/profile-pics/head_emerald.png" width="25" height="25" alt="head_emerald"/>
+            </span>
+            <div class="trackInfo">
+              <span class="trackName">
+                <span>King</span>
+              </span>
+              <span class="artistName">
+                <span>Tamara Bracknell</span>
+              </span>
+            </div>
+          </div>
+        </div>
+
+        <div id="nowPlayingCenter">
+          <div class="content playerControls">
+            <div class="buttons">
+              <button class="controlButton shuffle" title="Shuffle Button">
+                <img src="assets/images/icons/shuffle.png" alt="Shuffle"/>
+              </button>
+              <button class="controlButton previous" title="Previous Button">
+                <img src="assets/images/icons/previous.png" alt="Previous"/>
+              </button>
+              <button class="controlButton play" title="Play Button">
+                <img src="assets/images/icons/play.png" alt="Play"/>
+              </button>
+              <button class="controlButton pause" title="Pause Button" style="display: none;">
+                <img src="assets/images/icons/pause.png" alt="Pause"/>
+              </button>
+              <button class="controlButton next" title="Next Button">
+                <img src="assets/images/icons/next.png" alt="Next"/>
+              </button>
+              <button class="controlButton repeat" title="Repeat Button">
+                <img src="assets/images/icons/repeat.png" alt="Repeat"/>
+              </button>
+            </div>
+            <div class="playbackBar">
+              <span class="progressTime current">0.00</span>
+              <div class="progressBar">
+                <div class="progressBarBg">
+                  <div class="progress"></div>
+                </div>
+              </div>
+              <span class="progressTime remaining">0.00</span>
+            </div>
+          </div>
+        </div>
+
+        <div id="nowPlayingRight">
+
+        </div>
+      </div>
+
+    </div>
   </body>
 
 </html>
