3c2864979bcdaa9c8f87234d3b253ac2d612e2d2
Chuckz Okoye <tricelex@gmail.com>
2019-10-11 03:26:20 +0100

    Completed Left navgation

--- index.php
+++ index.php
@@ -16,75 +16,107 @@
   </head>
 
   <body>
+    <div class="mainContainer">
+      <div id="topContainer">
+        <div id="navBarContainer">
+          <nav class="navBar">
+            <a href="index.php" class="logo">
+              <img src="assets/images/icons/logo.png" alt="logo"/>
+            </a>
 
-    <div id="nowPlayingBarContainer">
-      <div id="nowPlayingBar">
+            <div class="group">
+              <div class="navItem">
+                <a href="search.php" class="navItemLink">Search
+                  <img src="assets/images/icons/search.png" class="icon" alt="Search">
+                </a>
+              </div>
+            </div>
 
-        <div id="nowPlayingLeft">
-          <div class="content">
-            <span class="albumLink">
-              <img class="albumArtwork" src="https://i.ytimg.com/vi/rb8Y38eilRM/maxresdefault.jpg"/>
-            </span>
-            <div class="trackInfo">
-              <span class="trackName">
-                <span>King</span>
+            <div class="group">
+              <div class="navItem">
+                <a href="browse.php" class="navItemLink">Browse</a>
+              </div> <div class="navItem">
+                <a href="yourMusic.php" class="navItemLink"> Your Music</a>
+              </div> <div class="navItem">
+                <a href="profle.php" class="navItemLink">King Chuckz</a>
+              </div>
+            </div>
+          </nav>
+
+        </div>
+      </div>
+
+      <div id="nowPlayingBarContainer">
+        <div id="nowPlayingBar">
+
+          <div id="nowPlayingLeft">
+            <div class="content">
+              <span class="albumLink">
+                <img class="albumArtwork" src="https://i.ytimg.com/vi/rb8Y38eilRM/maxresdefault.jpg"/>
               </span>
-              <span class="artistName">
-                <span>Tamara Bracknell</span>
-              </span>
+              <div class="trackInfo">
+                <span class="trackName">
+                  <span>Blow My Mind</span>
+                </span>
+                <span class="artistName">
+                  <span>Davido</span>
+                </span>
+              </div>
             </div>
           </div>
-        </div>
 
-        <div id="nowPlayingCenter">
-          <div class="content playerControls">
-            <div class="buttons">
-              <button class="controlButton shuffle" title="Shuffle Button">
-                <img src="assets/images/icons/shuffle.png" alt="Shuffle"/>
-              </button>
-              <button class="controlButton previous" title="Previous Button">
-                <img src="assets/images/icons/previous.png" alt="Previous"/>
-              </button>
-              <button class="controlButton play" title="Play Button">
-                <img src="assets/images/icons/play.png" alt="Play"/>
-              </button>
-              <button class="controlButton pause" title="Pause Button" style="display: none;">
-                <img src="assets/images/icons/pause.png" alt="Pause"/>
-              </button>
-              <button class="controlButton next" title="Next Button">
-                <img src="assets/images/icons/next.png" alt="Next"/>
-              </button>
-              <button class="controlButton repeat" title="Repeat Button">
-                <img src="assets/images/icons/repeat.png" alt="Repeat"/>
-              </button>
+          <div id="nowPlayingCenter">
+            <div class="content playerControls">
+              <div class="buttons">
+                <button class="controlButton shuffle" title="Shuffle Button">
+                  <img src="assets/images/icons/shuffle.png" alt="Shuffle"/>
+                </button>
+                <button class="controlButton previous" title="Previous Button">
+                  <img src="assets/images/icons/previous.png" alt="Previous"/>
+                </button>
+                <button class="controlButton play" title="Play Button">
+                  <img src="assets/images/icons/play.png" alt="Play"/>
+                </button>
+                <button class="controlButton pause" title="Pause Button" style="display: none;">
+                  <img src="assets/images/icons/pause.png" alt="Pause"/>
+                </button>
+                <button class="controlButton next" title="Next Button">
+                  <img src="assets/images/icons/next.png" alt="Next"/>
+                </button>
+                <button class="controlButton repeat" title="Repeat Button">
+                  <img src="assets/images/icons/repeat.png" alt="Repeat"/>
+                </button>
+              </div>
+              <div class="playbackBar">
+                <span class="progressTime current">0.00</span>
+                <div class="progressBar">
+                  <div class="progressBarBg">
+                    <div class="progress"></div>
+                  </div>
+                </div>
+                <span class="progressTime remaining">0.00</span>
+              </div>
             </div>
-            <div class="playbackBar">
-              <span class="progressTime current">0.00</span>
+          </div>
+
+          <div id="nowPlayingRight">
+            <div class="volumeBar">
+              <button class="controlButton volume" title="Volume Button">
+                <img src="assets/images/icons/volume.png" alt="Volume ">
+              </button>
               <div class="progressBar">
                 <div class="progressBarBg">
                   <div class="progress"></div>
                 </div>
               </div>
-              <span class="progressTime remaining">0.00</span>
             </div>
           </div>
         </div>
 
-        <div id="nowPlayingRight">
-          <div class="volumeBar">
-            <button class="controlButton volume" title="Volume Button">
-              <img src="assets/images/icons/volume.png" alt="Volume ">
-            </button>
-            <div class="progressBar">
-              <div class="progressBarBg">
-                <div class="progress"></div>
-              </div>
-            </div>
-          </div>
-        </div>
       </div>
 
     </div>
+
   </body>
 
 </html>
